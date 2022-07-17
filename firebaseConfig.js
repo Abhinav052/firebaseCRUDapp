8d279bf15d70f96b9527b749cb79e36f5d0c7fea
Abhinav052 <abhiabhinav535l@gmail.com>
2022-07-17 16:56:19 +0530

    final commit

---
+++ firebaseConfig.js
@@ -0,0 +1,20 @@
+
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCQXIBLWrRJCWZTBHjC65qyEE1xnrpzKnk",
+  authDomain: "crudapp-7996f.firebaseapp.com",
+  projectId: "crudapp-7996f",
+  storageBucket: "crudapp-7996f.appspot.com",
+  messagingSenderId: "804741045287",
+  appId: "1:804741045287:web:b21085ebcb9017907b0202"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
